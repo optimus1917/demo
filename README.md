fa4fb06e469b1ba7af92e5bf6639a2c1d28a83a2
grez1917 <optimus@Maranello.localdomain>
2021-10-06 13:51:48 +0700

    ini adalah commit dari cabang1

---
+++ README.md
@@ -0,0 +1,13 @@
+# demo
+Ini adalah pesan dari saya.
+echo "# demo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/optimus1917/demo.git
+git push -u origin main
+
+git remote add origin https://github.com/optimus1917/demo.git
+git branch -M main
+git push -u origin main
---
+++ tugas1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tugas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi my name is optimus");
+
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+            Console.WriteLine("Hallo " + aFriend);
+
+            Console.WriteLine($"My Friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"Hello {aFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
+
+            String songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+            Console.WriteLine(songLyrics.EndsWith("hello"));
+        }
+    }
+}
