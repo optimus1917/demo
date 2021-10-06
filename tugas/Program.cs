cbed56d3ad655bdb3cb75ea077c8dff9a9951ba8
grez1917 <optimus@Maranello.localdomain>
2021-10-06 15:12:01 +0700

    ini tugas modul pertama (1)

---
+++ tugas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string aFriend = "Bill";
+            string bFreind = "Nabilla";
+
+            Console.WriteLine("Hallo " + aFriend + " " + bFreind);
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+
+        }
+    }
+}
